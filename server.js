7c3793b2955d602e3265c78faf7debb729497294
jona1234559 <jona1234559@gmail.com>
2026-04-01 13:06:23 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -171,6 +171,8 @@ app.get("/buscar/:nombre", verificarAuth, async (req,res)=>{
 
 });
 
-app.listen(3000, ()=>{
-  console.log("API corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+  console.log("API corriendo");
 });
